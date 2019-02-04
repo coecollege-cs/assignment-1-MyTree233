6d05adb2d29ef05fe4e5331086743d9f8cfb98d1
MyTree233 <dyin@coe.edu>
2019-02-04 00:34:46 -0600

    Add the formal code of Sizeof

--- sizeof.c
+++ sizeof.c
@@ -13,6 +13,30 @@ int main()
    printf("Double value is %lf \n", dbl);
    printf("Octal value is %o \n", no);
    printf("Hexadecimal value is %x \n", no);
+   printf("Below this line is new Formal code for SIZE OF");
+
+
+   printf("==============================================================");
+   printf("===============================================================");
+
+
+   printf("The size of char is", sizeof(char));
+   printf("The size of int is", sizeof(int));
+   printf("The size of long is", sizeof(long));
+   printf("The size of float is", sizeof(float));
+   printf("The size of double is", sizeof(double));
+   printf("The size of unsigned int is ", sizeof(unsigned int));
+   printf("The size of unsigned char is ", sizeof(unsigned char));
+   printf("The size of unsigned long is ", sizeof(unsigned long));
+   printf("The size of int* is", sizeof(int*));
+   printf("The size of char* is", sizeof(char*));
+   printf("The size of long* is", sizeof(long*));
+   
+   printf("The explaination will update soon");
+
+
+
+
    return 0;
 }
 
